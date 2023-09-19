1a4ac1b0caf8b23ba1dc7444503740d542ecced3
Ryan Donahue <rdonahue@sbli.com>
2023-09-19 10:37:54 -0400

    remved quote from base domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Main() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Logo></Logo>
-      <QuickQuote></QuickQuote>
     </main>
   );
 }
