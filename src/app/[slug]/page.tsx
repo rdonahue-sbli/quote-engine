ca299d3d461164923b55765887b928a90925f551
Ryan Donahue <rdonahue@sbli.com>
2023-09-21 08:31:46 -0400

    revert test commits

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -6,56 +6,55 @@ import { Metadata, ResolvingMetadata } from "next";
 import { notFound } from "next/navigation";
 import { Logo } from "../_components/Logo";
 
-// export async function generateMetadata(
-//   {
-//     params,
-//   }: {
-//     params: { slug: string };
-//   },
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//   const [data] = await serverClient.wp.getPageBySlug(params.slug);
+export async function generateMetadata(
+  {
+    params,
+  }: {
+    params: { slug: string };
+  },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const [data] = await serverClient.wp.getPageBySlug(params.slug);
 
-//   return {
-//     title: data?.title?.rendered,
-//     description: data?.excerpt?.rendered,
-//   };
-// }
+  return {
+    title: data?.title?.rendered,
+    description: data?.excerpt?.rendered,
+  };
+}
 
 export default async function LandingPage({
   params: { slug },
 }: {
   params: { slug: string };
 }) {
-  // const [content] = await serverClient.wp.getPageBySlug(slug);
-  // if (!content?.acf) notFound();
+  const [content] = await serverClient.wp.getPageBySlug(slug);
+  if (!content?.acf) notFound();
 
-  // const [mainHeroMedia, secondaryHeroMedia] =
-  //   await serverClient.wp.getMediaByIds([
-  //     content?.acf.main_hero.image,
-  //     content?.acf.secondary_hero.image,
-  //   ]);
+  const [mainHeroMedia, secondaryHeroMedia] =
+    await serverClient.wp.getMediaByIds([
+      content?.acf.main_hero.image,
+      content?.acf.secondary_hero.image,
+    ]);
 
   return (
     <>
       <header className="flex">
         <div className="p-4 lg:container lg:mx-auto">
           <div className="w-56">
-            <h1>test</h1>
-            {/* <Logo></Logo> */}
+            <Logo></Logo>
           </div>
         </div>
       </header>
       <div className="flex flex-col gap-16">
-        {/* <Hero
+        <Hero
           content={content.acf.main_hero}
           image={mainHeroMedia}
           mode="light"
-        /> */}
+        />
         <div>
-          {/* <QuickQuote></QuickQuote> */}
+          <QuickQuote></QuickQuote>
         </div>
-        {/* <div className="flex flex-col items-center gap-8 lg:container lg:mx-auto line-clamp-2">
+        <div className="flex flex-col items-center gap-8 lg:container lg:mx-auto line-clamp-2">
           <h1 className="max-w-4xl text-3xl font-bold text-center lg:text-6xl text-sky-950 font-merriweather">
             {content.acf.main.title}
           </h1>
@@ -63,18 +62,18 @@ export default async function LandingPage({
             {content.acf.main.content}
           </p>
         </div>
-        <Reviews></Reviews> */}
-        {/* <Hero
+        <Reviews></Reviews>
+        <Hero
           content={content.acf.secondary_hero}
           image={secondaryHeroMedia}
           mode="dark"
           reverse
-        /> */}
-        {/* <div className="bg-sky-600 text-white py-8 text-xs text-opacity-80">
+        />
+        <div className="bg-sky-600 text-white py-8 text-xs text-opacity-80">
           <div className="flex flex-col items-center gap-8 lg:container lg:mx-auto">
             {content.acf.disclaimer}
           </div>
-        </div> */}
+        </div>
       </div>
     </>
   );
